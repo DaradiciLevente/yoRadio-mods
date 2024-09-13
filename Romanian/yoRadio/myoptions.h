b8739fb18dfcb01873fb255e34eefbb31bbd6e48
echosmart792 <echosmart792@gmail.com>
2024-09-13 14:58:34 +0300

    Add files via upload

---
+++ Romanian/yoRadio/myoptions.h
@@ -0,0 +1,13 @@
+#ifndef myoptions_h
+#define myoptions_h
+ 
+#define DSP_MODEL			DSP_ILI9488
+#define BRIGHTNESS_PIN			2
+
+//#define TS_MODEL			TS_MODEL_XPT2046
+//#define TS_HSPI			true
+//#define TS_CS			21
+
+//#define IR_PIN      15
+ 
+#endif
