a7b7332aaa9fc5ea489954004ab58b465b8c75b7
DaradiciLevente <echosmart792@gmail.com>
2024-09-15 11:58:39 +0300

    Add files via upload

--- Romanian/yoRadio/mytheme.h
+++ Romanian/yoRadio/mytheme.h
@@ -1,5 +1,5 @@
 // File created with YoRadio Theme Editor created by András Daradics
-// File last modified: 9/13/2024 2:32:13 PM
+// File last modified: 9/14/2024 10:45:52 PM
 // GitHub: https://github.com/andrasdaradici/YoRadio-Theme-Editor
 // Itch.io: https://andrasdaradici.itch.io/yoradio-theme-editor
 #ifndef _my_theme_h
@@ -8,29 +8,29 @@
 #define ENABLE_THEME
 #ifdef  ENABLE_THEME
 
-#define COLOR_BACKGROUND     0,  0,  91
-#define COLOR_STATION_NAME     0,  0,  255
+#define COLOR_BACKGROUND     133,  0,  0
+#define COLOR_STATION_NAME     255,  255,  255
 #define COLOR_STATION_BG    0,  0,  0
 #define COLOR_STATION_FILL    0,  0,  0
-#define COLOR_SNG_TITLE_1    255,  0,  0
+#define COLOR_SNG_TITLE_1    255,  71,  0
 #define COLOR_SNG_TITLE_2    255,  255,  0
-#define COLOR_WEATHER    0,  255,  255
-#define COLOR_VU_MAX    255,  0,  0
-#define COLOR_VU_MIN    0,  0,  255
-#define COLOR_CLOCK    249,  255,  255
-#define COLOR_CLOCK_BG    0,  0,  62
-#define COLOR_SECONDS    0,  255,  255
-#define COLOR_DAY_OF_W    252,  255,  0
-#define COLOR_DATE    0,  162,  241
-#define COLOR_BUFFER    157,  0,  79
+#define COLOR_WEATHER    0,  99,  255
+#define COLOR_VU_MAX    255,  255,  0
+#define COLOR_VU_MIN    255,  37,  0
+#define COLOR_CLOCK    255,  255,  0
+#define COLOR_CLOCK_BG    155,  0,  0
+#define COLOR_SECONDS    0,  99,  255
+#define COLOR_DAY_OF_W    0,  255,  66
+#define COLOR_DATE    255,  37,  0
+#define COLOR_BUFFER    0,  99,  255
 #define COLOR_IP    255,  255,  255
-#define COLOR_VOLUME_VALUE    255,  0,  0
-#define COLOR_RSSI    255,  238,  255
+#define COLOR_VOLUME_VALUE    255,  255,  255
+#define COLOR_RSSI    255,  255,  255
 #define COLOR_VOLBAR_OUT    255,  255,  0
-#define COLOR_VOLBAR_IN    0,  0,  255
+#define COLOR_VOLBAR_IN    0,  99,  255
 #define COLOR_DIGITS    100, 100, 255
-#define COLOR_DIVIDER    238,  0,  0
-#define COLOR_BITRATE    0,  0,  255
+#define COLOR_DIVIDER    255,  255,  255
+#define COLOR_BITRATE    255,  255,  255
 #define COLOR_HEAP    255, 168, 162
 #define COLOR_PL_CURRENT    0,  0,  0
 #define COLOR_PL_CURRENT_BG    91,  118,  255
