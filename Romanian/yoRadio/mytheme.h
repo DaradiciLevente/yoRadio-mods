dfc5063e5b20e994b0cdec523d6b509a253fc629
DaradiciLevente <echosmart792@gmail.com>
2024-09-17 21:52:02 +0300

    Add files via upload

--- Romanian/yoRadio/mytheme.h
+++ Romanian/yoRadio/mytheme.h
@@ -1,5 +1,5 @@
 // File created with YoRadio Theme Editor created by András Daradics
-// File last modified: 9/17/2024 7:06:24 PM
+// File last modified: 9/17/2024 9:50:45 PM
 // GitHub: https://github.com/andrasdaradici/YoRadio-Theme-Editor
 // Itch.io: https://andrasdaradici.itch.io/yoradio-theme-editor
 #ifndef _my_theme_h
@@ -10,36 +10,36 @@
 
 #define COLOR_BACKGROUND     0,  0,  128
 #define COLOR_STATION_NAME     255,  255,  0
-#define COLOR_STATION_BG    255,  0,  0
-#define COLOR_STATION_FILL    255,  0,  0
-#define COLOR_SNG_TITLE_1    255,  71,  0
+#define COLOR_STATION_BG    200,  0,  0
+#define COLOR_STATION_FILL    200,  0,  0
+#define COLOR_SNG_TITLE_1    255,  0,  0
 #define COLOR_SNG_TITLE_2    255,  255,  0
-#define COLOR_WEATHER    0,  99,  255
+#define COLOR_WEATHER    0,  255,  255
 #define COLOR_VU_MAX    255,  255,  0
-#define COLOR_VU_MIN    255,  0,  0
+#define COLOR_VU_MIN    255,  25,  0
 #define COLOR_CLOCK    255,  255,  0
 #define COLOR_CLOCK_BG    0,  0,  108
-#define COLOR_SECONDS    255,  0,  0
+#define COLOR_SECONDS    255,  25,  0
 #define COLOR_DAY_OF_W    0,  255,  255
-#define COLOR_DATE    255,  0,  0
-#define COLOR_BUFFER    0,  99,  255
+#define COLOR_DATE    0,  255,  255
+#define COLOR_BUFFER    40,  40,  168
 #define COLOR_IP    255,  255,  255
 #define COLOR_VOLUME_VALUE    255,  255,  255
 #define COLOR_RSSI    255,  255,  255
 #define COLOR_VOLBAR_OUT    255,  255,  0
-#define COLOR_VOLBAR_IN    0,  99,  255
+#define COLOR_VOLBAR_IN    0,  108,  255
 #define COLOR_DIGITS    100, 100, 255
 #define COLOR_DIVIDER    255,  255,  255
 #define COLOR_BITRATE    255,  255,  255
 #define COLOR_HEAP    255, 168, 162
-#define COLOR_PL_CURRENT    0,  0,  0
-#define COLOR_PL_CURRENT_BG    91,  118,  255
-#define COLOR_PL_CURRENT_FILL    91,  118,  255
-#define COLOR_PLAYLIST_0    255,  255,  255
-#define COLOR_PLAYLIST_1    255,  255,  255
-#define COLOR_PLAYLIST_2    255,  255,  255
-#define COLOR_PLAYLIST_3    255,  255,  255
-#define COLOR_PLAYLIST_4    255,  255,  255
+#define COLOR_PL_CURRENT    255,  255,  255
+#define COLOR_PL_CURRENT_BG    255,  0,  0
+#define COLOR_PL_CURRENT_FILL    255,  0,  0
+#define COLOR_PLAYLIST_0    255,  255,  0
+#define COLOR_PLAYLIST_1    0,  255,  255
+#define COLOR_PLAYLIST_2    255,  0,  0
+#define COLOR_PLAYLIST_3    255,  255,  0
+#define COLOR_PLAYLIST_4    0,  255,  255
 
 
 #endif
